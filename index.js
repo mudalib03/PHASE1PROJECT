f94b9212c053d2f15951934271be1f7a16d913ec
mudalib03 <mudalibabdulle17@gmail.com>
2025-03-28 14:27:36 +0300

    adding const for toggle button

--- index.js
+++ index.js
@@ -91,3 +91,22 @@ function displayRecommendations(movies) {
         recommendedDiv.appendChild(movieElement);
     });
 }
+
+const toggleButton = document.getElementById("theme-toggle");
+
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
