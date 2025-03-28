70875dfe33715a9c10f710d9a240c71a5c4f13a4
mudalib03 <mudalibabdulle17@gmail.com>
2025-03-28 13:41:44 +0300

    adding async function to search for movies

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 let watchHistory = [];
 let watchlist = [];
+
+
+async function searchMovies() {
+    const query = document.getElementById('search').value;
+    if (!query) return;
+    const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`);
+    const data = await res.json();
+    displayMovies(data.results);
+    updateWatchHistory(data.results);
+    getRecommendations();
+}
