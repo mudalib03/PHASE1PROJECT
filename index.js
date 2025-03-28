a9083e41bc75093b6046c8611f78fcf64dd2ffcf
mudalib03 <mudalibabdulle17@gmail.com>
2025-03-28 13:40:31 +0300

    adding const to fetch api key

---
+++ index.js
@@ -0,0 +1,5 @@
+const API_KEY = '944d364ae11b0639bcd80d85111280ae';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+let watchHistory = [];
+let watchlist = [];
